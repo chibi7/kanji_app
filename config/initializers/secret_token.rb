b3ec69f2d48a9670e207853191bba235d5ea9e67
vagrant <vagrant@packer-virtualbox-iso.(none)>
2014-05-24 15:43:25 +0000

    Structure of application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KanjiApp::Application.config.secret_token = 'cdce32e136661168980767c9890889a610a4da408adb947ddf91e4d29f168c212eead9694135908ba19395cf4939eb8b3c7724af835dc7816dacfd381678c6b8'
